7089f3f883ede283e8c48220a3d7ad0708e3694c
lichiguy <emiliano.reyes.423@gmail.com>
2025-07-03 23:42:40 -0600

    Ejercicios completos hasta 5.6

--- src/App.jsx
+++ src/App.jsx
@@ -103,14 +103,14 @@ const App = () => {
       {user && <div>
         <p>
           {user.name} logged in<button onClick={handleLogout}>log out</button>
-          <Togglable buttonLabel="create new blog" ref={blogFormRef}>
-            <BlogForm createBlog={addBlog} />
-          </Togglable>
         </p>
+        <Togglable buttonLabel="create new blog" ref={blogFormRef}>
+          <BlogForm createBlog={addBlog} />
+        </Togglable>
         {blogs.map((blog) => (
-          <>
-            <Blog key={blog.id} blog={blog} />
-          </>
+          <div key={blog.id}>
+            <Blog blog={blog} />
+          </div>
         ))}
       </div>}
     </div>
